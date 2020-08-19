487ebe0ea0348a805e32941a117454d41ff49c5d
vendetta <okta.pilopa@staff.itera.ac.id>
2020-08-19 21:54:19 +0700

    up

--- db_profil_azzahra.sql
+++ db_profil_azzahra.sql
@@ -11,6 +11,10 @@ MySQL - 10.4.13-MariaDB : Database - db_profil_azzahra
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_profil_azzahra` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_profil_azzahra`;
+
 /*Table structure for table `wp_commentmeta` */
 
 DROP TABLE IF EXISTS `wp_commentmeta`;
@@ -122,7 +126,7 @@ CREATE TABLE `wp_options` (
   PRIMARY KEY (`option_id`),
   UNIQUE KEY `option_name` (`option_name`),
   KEY `autoload` (`autoload`)
-) ENGINE=InnoDB AUTO_INCREMENT=580 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=581 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `wp_postmeta` */
 
