74532d959cab300a588e700c80e047562c5d18a2
vendetta <okta.pilopa@staff.itera.ac.id>
2020-08-19 10:29:16 +0700

    up

--- db_profil_azzahra.sql
+++ db_profil_azzahra.sql
@@ -136,7 +136,7 @@ CREATE TABLE `wp_postmeta` (
   PRIMARY KEY (`meta_id`),
   KEY `post_id` (`post_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=9270 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9272 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `wp_posts` */
 
